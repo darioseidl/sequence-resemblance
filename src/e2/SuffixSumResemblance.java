e85d276d2be42f6ea4468697354350b086925a26
Dario Seidl <darioseidl@fastmail.fm>
2019-02-17 03:51:22 +0100

    cleanup

--- src/e2/SuffixSumResemblance.java
+++ src/e2/SuffixSumResemblance.java
@@ -9,16 +9,23 @@ public class SuffixSumResemblance {
         System.out.println(suffixSumResemblance("ababaa"));
     }
 
+//    public static Object suffixSumResemblance(String sequence) {
+//        return IntStream
+//                .range(0, sequence.length())
+//                .map(i -> {
+//                    int resemblance = Resemblance.resemblance(sequence, sequence.substring(i, sequence.length()));
+//                    System.out.println("sequence: " + sequence);
+//                    System.out.println("substring: " + sequence.substring(i, sequence.length()));
+//                    System.out.println(resemblance);
+//                    return resemblance;
+//                }).sum();
+//
+//    }
+
     public static Object suffixSumResemblance(String sequence) {
         return IntStream
                 .range(0, sequence.length())
-                .map(i -> {
-                    int resemblance = Resemblance.resemblance(sequence, sequence.substring(i, sequence.length()));
-                    System.out.println("sequence: " + sequence);
-                    System.out.println("substring: " + sequence.substring(i, sequence.length()));
-                    System.out.println(resemblance);
-                    return resemblance;
-                }).sum();
+                .map(i -> Resemblance.resemblance(sequence, sequence.substring(i, sequence.length()))).sum();
 
     }
 }
