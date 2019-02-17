335086a4e81b007b2ac5318b73abdcf74a20139d
Dario Seidl <darioseidl@fastmail.fm>
2019-02-17 10:25:03 +0100

    remove old code

--- src/e2/SuffixSumResemblance.java
+++ src/e2/SuffixSumResemblance.java
@@ -12,17 +12,4 @@ public class SuffixSumResemblance {
                 .map(i -> new Resemblance().resemblance(sequence, sequence.substring(i, sequence.length()))).sum();
     }
 
-//    public int suffixSumResemblance(String sequence) {
-//        return IntStream
-//                .range(0, sequence.length())
-//                .map(i -> {
-//                    int resemblance = Resemblance.resemblance(sequence, sequence.substring(i, sequence.length()));
-//                    System.out.println("sequence: " + sequence);
-//                    System.out.println("substring: " + sequence.substring(i, sequence.length()));
-//                    System.out.println(resemblance);
-//                    return resemblance;
-
-//                }).sum();
-
-//    }
 }
